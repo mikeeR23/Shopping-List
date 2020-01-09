be9bf64f607835d2278f423836506a97bebd4be7
Michael Ramirez <Ramirez1815@yahoo.com>
2020-01-09 17:11:28 -0500

    Added html

--- ShoppingList/src/app/recipes/recipe-list/recipe-list.component.ts
+++ ShoppingList/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,6 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe('Spaghetti', 'this is a test', 'https://cdn.pixabay.com/photo/2016/08/19/09/24/spaghetti-1604836_960_720.jpg'), 
+    new Recipe('Spaghetti', 'this is a test', 'https://cdn.pixabay.com/photo/2016/08/19/09/24/spaghetti-1604836_960_720.jpg')
+
+  ];
 
   constructor() { }
 
